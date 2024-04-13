a505dff4f72638598fb55dc1ee0819a10039a3e3
Prateek Giridhar <pgiridha@Prateeks-MacBook-Pro.local>
2024-04-13 01:51:08 -0400

    Added OTP and other merchant changes

--- src/Components/Dashboard/External/Merchant/Profile.jsx
+++ src/Components/Dashboard/External/Merchant/Profile.jsx
@@ -1,99 +1,175 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { jwtDecode } from "jwt-decode";
 
 export function Profile({ token }) {
-    const [userDetails, setUserDetails] = useState({});
-    const [userAttributes, setUserAttributes] = useState({});
-    const [accountDetails, setAccountDetails] = useState({});
-    const [editMode, setEditMode] = useState(false);
-    const [updatedAddress, setUpdatedAddress] = useState('');
-    const [updatedPhoneNumber, setUpdatedPhoneNumber] = useState('');
-    const [userId, setUserId] = useState(null);
-    const [accountId, setAccountId] =useState(null);
+  const [userDetails, setUserDetails] = useState({});
+  const [userAttributes, setUserAttributes] = useState({});
+  const [accountDetails, setAccountDetails] = useState({});
+  const [editMode, setEditMode] = useState(false);
+  const [updatedAddress, setUpdatedAddress] = useState("");
+  const [updatedPhoneNumber, setUpdatedPhoneNumber] = useState("");
+  const [userId, setUserId] = useState(null);
 
-    useEffect(() => {
-        const fetchUserDetails = async () => {
-            try {
-                const token = localStorage.getItem('authToken');
-                const decodedToken = jwtDecode(token);
-                
-                setUserId(decodedToken.userId);
-                setAccountId(decodedToken.accountId);
-                
-                const response = await fetch(`${process.env.REACT_APP_API_URL}/user/account/${accountId}`, {
-                    headers: { 'Authorization': token }
-                });
-                
-                if (!response.ok) throw new Error('Failed to fetch user details');
-                
-                const data = await response.json();
-                
-                setAccountDetails(data);
-                setUserDetails(data.user);
-                setUserAttributes(data.user.attributes);
-                setUpdatedAddress(data.address);
-                setUpdatedPhoneNumber(data.phone_number);
-            
-            } catch (error) {
-                console.error('Error fetching user details:', error);
-            }
-        };
-        fetchUserDetails();
-    }, [accountId, token]);
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      try {
+        const token = localStorage.getItem("authToken");
+        const decodedToken = jwtDecode(token);
 
-    const handleUpdateDetails = async () => {
-        
-        try {
-            const response = await fetch(`${process.env.REACT_APP_API_URL}/user/profile/${userId}`, {
-                method: 'PATCH',
-                headers: {
-                    'Authorization': token,
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    attributes: {
-                        address: updatedAddress,
-                        phone_number: updatedPhoneNumber
-                    }
-                }),
-            });
-            if (!response.ok) throw new Error('Failed to update user details');
-            alert('User details updated successfully!');
-            setEditMode(false);
-        } catch (error) {
-            console.error('Error updating user details:', error);
-        }
+        setUserId(decodedToken.userId);
+
+        const accountResponse = await fetch(
+          `${process.env.REACT_APP_API_URL}/user/account/${decodedToken.accountId}`,
+          {
+            headers: { Authorization: token },
+          }
+        );
+
+        if (!accountResponse.ok) throw new Error("Failed to fetch user details");
+
+        const accountData = await accountResponse.json();
+
+        setAccountDetails(accountData);
+        setUserDetails(accountData.user);
+        setUserAttributes(accountData.user.attributes);
+        setUpdatedAddress(accountData.address);
+        setUpdatedPhoneNumber(accountData.phone_number);
+
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+      }
     };
+    fetchUserDetails();
+  }, []);
 
-    return (
-        <div>
-            <h2>User Profile</h2>
-            {!editMode ? (
-                <div>
-                    <p>Username: {userDetails.username}</p>
-                    <p>Email: {userDetails.email}</p>
-                    <p>Name: {userAttributes.business_name}</p>
-                    <p>Date of Birth: {userAttributes.owner_dob}</p>
-                    <p>Gender: {userAttributes.owner_gender}</p>
-                    <p>EIN: {userAttributes.ein}</p>
-                    <p>SSN: {userAttributes.owner_ssn}</p>
-                    <p>Address: {userAttributes.address}</p>
-                    <p>Phone Number: {userAttributes.business_phone}</p>
-                    <p>Role: {userDetails.role}</p>
-                    <p>Account Number: {accountDetails.accountNumber}</p> 
-                    <button onClick={() => setEditMode(true)}>Edit</button>
-                </div>
-            ) : (
-                <div>
-                    <h2>Edit Profile</h2>
-                    <label>Address:</label>
-                    <input type="text" value={updatedAddress} onChange={e => setUpdatedAddress(e.target.value)} />
-                    <label>Phone Number:</label>
-                    <input type="number" value={updatedPhoneNumber} onChange={e => setUpdatedPhoneNumber(e.target.value)} />
-                    <button onClick={handleUpdateDetails}>Save</button>
-                    <button onClick={() => setEditMode(false)}>Cancel</button>
-                </div>
-            )}
-        </div>
-    );
+  const handleUpdateDetails = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/user/profile/${userId}`,
+        {
+          method: "PATCH",
+          headers: {
+            Authorization: token,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            attributes: {
+              address: updatedAddress,
+              phone_number: updatedPhoneNumber,
+            },
+          }),
+        }
+      );
+      if (!response.ok) throw new Error("Failed to update user details");
+      alert("User details updated successfully!");
+      setEditMode(false);
+    } catch (error) {
+      console.error("Error updating user details:", error);
+    }
+  };
+
+  return (
+    <div className="profile">
+      {!editMode ? (
+        <center>
+          <h2>User Profile</h2>
+          <br />
+          <table>
+            <tr>
+              <th>Username</th>
+              <td>{userDetails.username}</td>
+            </tr>
+            <tr>
+              <th>Email</th>
+              <td>{userDetails.email}</td>
+            </tr>
+            <tr>
+              <th>First Name</th>
+              <td>{userAttributes.first_name}</td>
+            </tr>
+            <tr>
+              <th>Middle Name</th>
+              <td>{userAttributes.middle_name || "-"}</td>
+            </tr>
+            <tr>
+              <th>Last Name</th>
+              <td>{userAttributes.last_name}</td>
+            </tr>
+            <tr>
+              <th>Date of Birth</th>
+              <td>{userAttributes.date_of_birth}</td>
+            </tr>
+            <tr>
+              <th>Gender</th>
+              <td>{userAttributes.gender}</td>
+            </tr>
+            <tr>
+              <th>SSN</th>
+              <td>{userAttributes.ssn}</td>
+            </tr>
+            <tr>
+              <th>Address</th>
+              <td>{userAttributes.address}</td>
+            </tr>
+            <tr>
+              <th>Phone Number</th>
+              <td>{userAttributes.phone_number}</td>
+            </tr>
+            <tr>
+              <th>Role</th>
+              <td>{userDetails.role}</td>
+            </tr>
+            <tr>
+              <th>Account Number</th>
+              <td>{accountDetails.accountNumber}</td>
+            </tr>
+          </table>
+          <br />
+          <button className="edit-profile" onClick={() => setEditMode(true)}>
+            Edit
+          </button>
+        </center>
+      ) : (
+        <center>
+          <h2>Edit Profile</h2>
+          <br />
+          <table>
+            <tr>
+              <th>Address</th>
+              <td>
+                <input
+                  type="text"
+                  value={updatedAddress}
+                  onChange={(e) => setUpdatedAddress(e.target.value)}
+                />
+              </td>
+            </tr>
+            <tr>
+              <th>Phone Number</th>
+              <td>
+                <input
+                  type="tel"
+                  value={updatedPhoneNumber}
+                  onChange={(e) => setUpdatedPhoneNumber(e.target.value)}
+                />
+              </td>
+            </tr>
+            {/* <tr>
+              <th>Email</th>
+              <td>
+                <input
+                  type="text"
+                  value={updateEmail}
+                  onChange={(e) => setUpdateEmail(e.target.value)}
+                />
+              </td>
+            </tr> */}
+          </table>
+          <br />
+          <button onClick={handleUpdateDetails}>Save</button>
+          <button onClick={() => setEditMode(false)}>Cancel</button>
+        </center>
+      )}
+    </div>
+  );
 }
--- src/Components/Dashboard/External/Merchant/TransactionHistory.jsx
+++ src/Components/Dashboard/External/Merchant/TransactionHistory.jsx
@@ -1,38 +1,80 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-export function TransactionHistory({ token }) {
-    const [transactions, setTransactions] = useState([]);
+export function TransactionHistory() {
+  const [transactions, setTransactions] = useState([]);
 
-    useEffect(() => {
-        const fetchTransactions = async () => {
-            try {
-                const response = await fetch(`${process.env.REACT_APP_API_URL}/transaction/${token}`, {
-                    headers: { 'Authorization': token }
-                });
-                if (!response.ok) throw new Error('Failed to fetch transactions');
-                const data = await response.json();
-                setTransactions(data.transactions);
-            } catch (error) {
-                console.error('Error fetching transactions:', error);
-            }
-        };
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      try {
+        const token = localStorage.getItem("authToken");
 
-        fetchTransactions();
-    }, [token]);
+        if (!token) {
+          console.error("No auth token available");
+          return;
+        }
 
-    return (
-        <div>
-            <h2>Transaction History</h2>
-            <ul>
-                {transactions.map((transaction, index) => (
-                    <li key={index} style={{ color: transaction.type === 'CREDIT' ? 'green' : 'red' }}>
-                        <p>Date: {transaction.date}</p>
-                        <p>Type: {transaction.type}</p>
-                        <p>Amount: ${transaction.amount}</p>
-                        <p>Description: {transaction.description}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+        const response = await fetch(
+          `${process.env.REACT_APP_API_URL}/transaction/myTxns`,
+          {
+            method: "GET",
+            headers: { Authorization: token },
+          }
+        );
+
+        if (!response.ok) throw new Error("Failed to fetch transactions");
+
+        const data = await response.json();
+
+        setTransactions(data.transactions);
+      } catch (error) {
+        console.error("Error fetching transactions:", error);
+      }
+    };
+
+    fetchTransactions();
+  }, []);
+
+  return (
+    <div className="transaction-history">
+      <h2>Transaction History</h2>
+      <br />
+      <table>
+        <tr>
+          <th>Type</th>
+          <th>From</th>
+          <th>To</th>
+          <th>Amount</th>
+        </tr>
+        {transactions.length > 0 ? (
+          transactions.map((transaction, index) => (
+            <tr key={index}>
+              <td>{transaction.type}</td>
+              <td>{transaction.from}</td>
+              <td>{transaction.to}</td>
+              <td>{transaction.amount}</td>
+            </tr>
+          ))
+        ) : (
+          <p>No transactions found</p>
+        )}
+      </table>
+    </div>
+
+    // <div>
+    //   <h2>Transaction History</h2>
+    //   <ul>
+    //     {transactions.map((transaction, index) => (
+    //       <li
+    //         key={index}
+    //         style={{ color: transaction.type === "CREDIT" ? "green" : "red" }}
+    //       >
+    //         <p>Date: {transaction.date}</p>
+    //         <p>Type: {transaction.type}</p>
+    //         <p>Amount: ${transaction.amount}</p>
+    //         <p>Description: {transaction.description}</p>
+    //       </li>
+    //     ))}
+    //   </ul>
+    // </div>
+  );
 }
