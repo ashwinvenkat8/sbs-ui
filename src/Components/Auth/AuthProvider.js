10aad033a52ffa5972524beae54a4f0f30ca1596
Venkatakrishnan, Ashwin <ashvenk@iu.edu>
2024-04-14 02:54:43 -0400

    cleanup

--- src/Components/Auth/AuthProvider.js
+++ src/Components/Auth/AuthProvider.js
@@ -1,26 +1,8 @@
-// import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
-// import { jwtDecode } from 'jwt-decode';
 
 export const useAuth = () => {
     const navigate = useNavigate();
-    // const [isLoggedIn, setIsLoggedIn] = useState(false);
-    // const [userRole, setUserRole] = useState(null);
-    // const [userId, setUserId] = useState(null);
-
-    // useEffect(() => {
-    //     const token = localStorage.getItem('authToken');
-    //     const decodeToken = jwtDecode(token);
-    //     const role = decodeToken.role;
-    //     const userid = decodeToken.userId;
-
-    //     setIsLoggedIn(!!token);
-    //     setUserRole(role);
-    //     setUserId(userid);
-    // }, [isLoggedIn, userId]);
-
     const handleLogout = async () => {
-
         try {
             const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/logout`, {
                 method: 'POST',
@@ -39,10 +21,7 @@ export const useAuth = () => {
 
         // Clear local storage and update state
         localStorage.removeItem('authToken');
-        // setIsLoggedIn(false);
-        // setUserRole(null);
-        alert('You are being redirected to Home Page');
-        navigate("/");
+        navigate("/login");
     };
 
     return { handleLogout };
