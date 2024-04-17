58ff41c06fabb61ebe6d61dc693fa07ea0c8de89
Venkatakrishnan, Ashwin <ashvenk@iu.edu>
2024-04-17 01:02:12 -0400

    cleanup

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,19 +1,13 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-  const navigate = useNavigate();
-
   return (
     <div className="error-container">
       <div className="error-banner-container">
         <div className="error-text-section">
-          <h2 className="secondary-heading">
-            Page Not Found
-          </h2>
-          <p className="secondary-text">
-            404
-          </p>
+          <h2 className="secondary-heading">Page Not Found</h2>
+          <p className="secondary-text">404</p>
           <div className="auth-buttons">
             <Link className="link home" to="/">&lt; Home</Link>
           </div>
